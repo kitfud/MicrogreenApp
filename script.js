6728b15dc3427d99f33580f914cbb035187c78e6
Kit Fuderich <kitfuderich@gmail.com>
2020-11-06 23:18:31 +0000

    Interactive graphic for microgreen mat size

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let main = document.getElementById("data")
-let dataVar ="nope";
+let main = document.getElementById("data");
+let size = document.getElementById('size-input');
+let canvas = document.getElementById("canvas")
+let context = canvas.getContext("2d")
+context.fillStyle = "green"
+context.strokeStyle = "brown"
+let sizeVal= 1;
+let dataVar;
+grabData();
 
 function grabData(){
 fetch('https://api.npoint.io/9c28b9b051994bba6e16')
@@ -7,12 +14,25 @@ fetch('https://api.npoint.io/9c28b9b051994bba6e16')
   .then(data => {
   dataVar = data;
   })
-  .then(data => console.log((dataVar)))
+  //.then(data => console.log((dataVar)))
   .catch(error=>
-    console.log("error:"+ error)
+    console.log("Error occured:"+ error)
   )
 }
-grabData()
+
+size.onchange = function(){
+context.clearRect(0,0,canvas.width,canvas.height);
+context.fillRect(10,
+10,380*size.value,180)
+}
+
+function square(size){
+context.fillRect(10,
+10,380*size,180)
+}
+
+square(sizeVal)
+
 
 
 
